666be883476890ee5dd8f6fba1b9fa71102d639e
Daniel Alexssander <danielrossinatti15@gmail.com>
2024-04-25 13:29:01 -0300

    Add files via upload

--- src/components/MainContentImage.tsx
+++ src/components/MainContentImage.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import config from '../config/index.json';
+
 const MainContentImage = () => {
+  const { mainContent } = config;
+
   return (
     <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img
         className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        src="https://img.freepik.com/fotos-gratis/labrador-mentir-grama-verde-com-bola_23-2148045803.jpg?t=st=1713988486~exp=1713992086~hmac=ec9f629d646a93df3f98639fea801393eff6b797a78d780b1e0d9208c24136ac&w=1380"
+        src={mainContent.img}
         alt="happy team image"
       />
     </div>
